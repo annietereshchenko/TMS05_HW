23a5c930d74f3f8b295a9bf3a604ed64b2a54534
Anna Tereshchenko <annie.tereshchenko@gmail.com>
2022-04-10 18:16:55 +0300

    add tests for calculator

---
+++ calculator/calculator.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    def sum(self, first_number: int, second_number: int) -> int:
+        return first_number + second_number
+
+    def subtraction(self, first_number: int, second_number: int) -> int:
+        return first_number - second_number
+
+    def multiplication(self, first_number: int, second_number: int) -> int:
+        return first_number * second_number
+
+    def division(self, first_number: int, second_number: int) -> float:
+        return first_number / second_number
---
+++ test_module/test_unit/test_calculator.py
@@ -0,0 +1,50 @@
+import unittest
+from calculator import Calculator
+
+
+class TestCalculatorPositive(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calc = Calculator()
+
+    def test_sum(self):
+        self.assertEqual(self.calc.sum(5, 4), 9)
+
+    def test_sub(self):
+        self.assertEqual(self.calc.subtraction(10, 7), 3)
+
+    def test_multiple(self):
+        self.assertEqual(self.calc.multiplication(3, 5), 15)
+
+    def test_div(self):
+        self.assertEqual(self.calc.division(10, 2), 5)
+
+    def test_div_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.division(12, 0)
+
+
+class TestCalculatorNegative(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calc = Calculator()
+
+    @unittest.expectedFailure
+    def test_failed_sum(self):
+        self.assertEqual(self.calc.sum(4, 2), 7)
+
+    @unittest.expectedFailure
+    def test_failed_sub(self):
+        self.assertEqual(self.calc.subtraction(12, 2), 9)
+
+    @unittest.expectedFailure
+    def test_failed_multiple(self):
+        self.assertEqual(self.calc.multiplication(5, 4), 3)
+
+    @unittest.expectedFailure
+    def test_failed_div(self):
+        self.assertEqual(self.calc.division(12, 6), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
