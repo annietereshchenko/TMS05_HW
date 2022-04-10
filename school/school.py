34b6996309595fef8bc36a96ddb53506146b741f
Anna Tereshchenko <annie.tereshchenko@gmail.com>
2022-04-10 22:50:02 +0300

    add tests for school module

---
+++ school/school.py
@@ -0,0 +1,52 @@
+"""""
+Создайте класс Students, содержащий поля: фамилия и инициалы, номер группы,
+успеваемость (массив из пяти элементов).
+Создать класс School:
+Добавить возможно для добавления студентов в школу
+Добавить возможность вывода фамилий и номеров групп студентов,
+имеющих оценки, равные только 5 или 6.
+Добавить возможность вывода учеников заданной группы
+Добавить возможность вывода учеников претендующих на автомат(средний балл >= 7)
+"""""
+
+
+class Students:
+
+    def __init__(self, surname: str, group_number: int, estimates: list):
+        self.surname = surname
+        self.group_number = group_number
+        self.estimates = estimates
+
+
+class School:
+
+    def __init__(self):
+        self.students = list()
+
+    def add_student(self, student):
+        self.students.append(student)
+
+    def show_student_with_marks(self, *marks):
+        students_list = []
+        for student in self.students:
+            if all([mark in marks for mark in student.estimates]):
+                print(f"Student {student.surname} in the "
+                      f"{student.group_number} group has only {marks} marks")
+                students_list.append(student)
+        return students_list
+
+    def show_student_with_group(self, group: int):
+        student_list = []
+        for student in self.students:
+            if student.group_number == group:
+                print(f"Student {student.surname} in the group {group}")
+                student_list.append(student)
+        return student_list
+
+    def show_students_with_automat(self, average_number_for_automat: int):
+        students_with_automat = [student for student in self.students
+                                  if sum(student.estimates) / len(student.estimates) >= average_number_for_automat]
+
+        for student in students_with_automat:
+            print(f"Student {student.surname} has automat")
+        return students_with_automat
---
+++ test_module/test_unit/test_school.py
@@ -0,0 +1,77 @@
+import unittest
+from school import Students, School
+
+
+class TestForSchool(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestForSchool, self).__init__(*args, **kwargs)
+
+    def setUp(self) -> None:
+        self.school = School()
+        self.dzima = Students("Sadouski", 1, [5, 6, 5, 5, 6])
+        self.nikita = Students("Ronaldo", 1, [7, 7, 7, 7, 7])
+        self.sergey = Students("Sheva", 2, [8, 8, 8, 8, 8])
+        self.artem = Students("Sosna", 2, [2, 10, 5, 6, 2])
+        self.sasha = Students("Saga", 1, [6, 6, 6, 6, 6])
+
+    def test_get_students_with_certain_marks(self):
+        self.school.add_student(self.dzima)
+        self.school.add_student(self.nikita)
+        result = self.school.show_student_with_marks(5, 6)
+        self.assertEqual(len(result), 1)
+
+    def test_get_students_with_one_certain_mark(self):
+        """""
+        Здесь возьмем студента sasha, у которого все оценки только 6 в списке.
+        Проверять студента, у которого только 5 в списке, наверное, не имеет
+        смысла
+        """""
+        self.school.add_student(self.sasha)
+        result = self.school.show_student_with_marks(5, 6)
+        self.assertEqual(len(result), 1)
+
+    def test_get_students_with_not_all_certain_marks(self):
+        """""
+        Здесь возьмем студента artem, у которого есть оценки 5 и 6 в списке,
+        но так же у него есть другие оценки
+        """""
+        self.school.add_student(self.artem)
+        result = self.school.show_student_with_marks(5, 6)
+        self.assertEqual(len(result), 0)
+
+    def test_get_students_with_certain_marks_empty_school(self):
+        result = self.school.show_student_with_marks(5, 6)
+        self.assertEqual(len(result), 0)
+
+    def test_get_students_with_group(self):
+        self.school.add_student(self.sergey)
+        self.school.add_student(self.artem)
+        self.school.add_student(self.nikita)
+        result = self.school.show_student_with_group(2)
+        self.assertEqual(len(result), 2)
+
+    def test_get_students_with_group_empty_school(self):
+        result = self.school.show_student_with_group(1)
+        self.assertEqual(len(result), 0)
+
+    def test_get_students_with_automat(self):
+        """""
+        Здесь воспользуемся методом граничных значений
+        Средняя оценка для автомата - 7
+        Возьмем студентов со следующим средним баллом -
+        6, 7 и 8. Единственное, я сомневаюсь, 
+        надо ли писать три разных теста на каждое значение
+        """""
+        self.school.add_student(self.sergey)
+        self.school.add_student(self.nikita)
+        self.school.add_student(self.artem)
+        result = self.school.show_students_with_automat(7)
+        self.assertEqual(len(result), 2)
+
+    def test_get_students_with_automat_empty_school(self):
+        result = self.school.show_students_with_automat(7)
+        self.assertEqual(len(result), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
